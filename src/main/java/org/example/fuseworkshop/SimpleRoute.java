a191d0d835218a427ce5075dd19adf322ac925eb
gwrdev <31447106+gwrdev@users.noreply.github.com>
2018-06-08 19:51:14 +0100

    Adding queuing changes

--- src/main/java/org/example/fuseworkshop/SimpleRoute.java
+++ src/main/java/org/example/fuseworkshop/SimpleRoute.java
@@ -1,6 +1,10 @@
 package org.example.fuseworkshop;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+
+import static org.apache.camel.Exchange.FILE_NAME;
+import static org.apache.camel.Exchange.HTTP_RESPONSE_CODE;
 
 public class SimpleRoute extends RouteBuilder {
 
@@ -8,13 +12,36 @@ public class SimpleRoute extends RouteBuilder {
 
     @Override public void configure() throws Exception {
         from("timer://foo?period=5000").routeId("simpleRoute")
-                .setBody().constant("Hello "+ name)
+                .setBody().constant("Goodbye "+ name)
                 .log(">>> ${body}")
                 ;
+        from("file://input?include=.*\\.txt").routeId("fileProc")
+                .log(">>> ${body}")
+                .to("activemq:queue:fileQueue")
+                ;
+
+        restConfiguration()
+                .component("restlet")
+                .host("localhost")
+                .port("9095")
+                .bindingMode(RestBindingMode.off);
+
+        rest("/submission")
+            .post("/").consumes("text/plain")
+            .route().routeId("restIn")
+                .to("activemq:queue:submission")
+                .setHeader(HTTP_RESPONSE_CODE, constant(200));
+
+        from("activemq:queue:submission").routeId("restPRoc")
+                .log("s")
+                .convertBodyTo(String.class)
+                .to("pdf:create")
+                .setHeader(FILE_NAME, constant ("test-${date:now:yyyyMMdd-HHmmss}.pdf"))
+                .to("file://output")
+          ;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 }
-
