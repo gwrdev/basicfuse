4123168c1d80a0600f49d81c706d39b2d0dc6bef
gwrdev <gwrdev@home.com>
2018-06-09 14:28:55 +0100

    Set AMQ message to InOnly

--- src/main/java/org/example/fuseworkshop/SimpleRoute.java
+++ src/main/java/org/example/fuseworkshop/SimpleRoute.java
@@ -2,6 +2,7 @@ package org.example.fuseworkshop;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.ExchangePattern;
 
 import static org.apache.camel.Exchange.FILE_NAME;
 import static org.apache.camel.Exchange.HTTP_RESPONSE_CODE;
@@ -27,17 +28,24 @@ public class SimpleRoute extends RouteBuilder {
                 .bindingMode(RestBindingMode.off);
 
         rest("/submission")
-            .post("/").consumes("text/plain")
-            .route().routeId("restIn")
-                .to("activemq:queue:submission")
-                .setExchangePattern(ExchangePattern.InOnly)
-                .setHeader(HTTP_RESPONSE_CODE, constant(200));
+            .post("/new").consumes("text/plain")
+            .to("direct:restNew")
+            .post("/test").consumes("text/plain")
+            .to("direct:restTest");
 
-        from("activemq:queue:submission").routeId("restProc")
+	from("direct:restNew").routeId("restNew")
+		.log("Rest call to New")
+                .to(ExchangePattern.InOnly, "activemq:queue:submission");
+	from("direct:restTest").routeId("restTest")
+		.log("Rest call to Test")
+		.setBody().simple("Returning ${body}");
+
+        from("activemq:queue:submission").routeId("restFromQueue")
                 .log("PDF Taken from Queue")
                 .convertBodyTo(String.class)
                 .to("pdf:create")
-                .setHeader(FILE_NAME, constant ("test-${date:now:yyyyMMdd-HHmmss}.pdf"))
+                //.setHeader(FILE_NAME, constant ("test-${date:now:yyyyMMdd-HHmmss}.pdf"))
+		.setHeader(FILE_NAME, constant ("test.pdf"))
                 .to("file://output")
           ;
     }
